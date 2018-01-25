1458ce14c16f51a290dcb387afebfb2ced9470f2
Brandon Liu <brandon.slipjet@gmail.com>
2018-01-25 11:45:54 -0800

    Adding sass css partials

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('goodbye frien');
+// Built by LucyBot. www.lucybot.com
+var url = "https://api.nytimes.com/svc/topstories/v2/home.json";
+url += '?' + $.param({
+  'api-key': "033cf8ec92ed4c98b1eb0c9e33364101"
+});
+
+$.ajax({
+  url: url,
+  method: 'GET',
+}).done(function(result) {
+  console.log(result);
+}).fail(function(err) {
+  throw err
+});
