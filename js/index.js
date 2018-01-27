dd28681ab1445a0db86cac979972060c8d9f78cf
Brandon Liu <brandon.slipjet@gmail.com>
2018-01-27 09:57:42 -0800

    fetching data results titles

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
-// Built by LucyBot. www.lucybot.com
-var url = "https://api.nytimes.com/svc/topstories/v2/home.json";
+$('#options').on('change', function(){
+  var value = $('#options').val()
+
+  var url = "https://api.nytimes.com/svc/topstories/v2/" + value + ".json";
 url += '?' + $.param({
   'api-key': "033cf8ec92ed4c98b1eb0c9e33364101"
 });
@@ -7,11 +9,27 @@ url += '?' + $.param({
 $.ajax({
   url: url,
   method: 'GET',
-}).done(function(data) {
-  console.log(data.results);
-}).fail(function(err) {
+})
+  .done(function(data) {
+    
+
+   for (var i = 0; i < 12; i++) {
+
+   $('.storygrid').append("<div>"
+    + data.results [i].multimedia[0] +//picture
+    // + data.results [i].title  + //title
+  "</div>"
+  
+   );
+  }
+
+ console.log(data) 
+  
+
+ 
+ })
+
+.fail(function(err) {
   throw err
+  });
 });
-
-
-
